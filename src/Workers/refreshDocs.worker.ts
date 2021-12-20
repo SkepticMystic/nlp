6b8ea7605543d97e41fd1d7fbd90d27a716b6fd4
Ross Keenan <rossk29@gmail.com>
2021-12-20 10:28:40 +0200

    refactor: :fire: Worker cannot refreshDocs

--- src/Workers/refreshDocs.worker.ts
+++ src/Workers/refreshDocs.worker.ts
@@ -1,4 +1,18 @@
-self.onmessage = async function (e) {
-	const parsedJSON = await JSON.parse(e.data);
-	self.postMessage(parsedJSON);
-};
+// import { WinkMethods } from "wink-nlp";
+
+// self.onmessage = async function (e) {
+// 	console.log(e);
+// 	const docs = {};
+// 	const {
+// 		contentObj,
+// 		winkModel,
+// 	}: { contentObj: { [path: string]: string }; winkModel: WinkMethods } =
+// 		e.data;
+
+// 	console.log({ contentObj });
+
+// 	for (const path in contentObj) {
+// 		docs[path] = winkModel.readDoc(contentObj[path]);
+// 	}
+// 	self.postMessage(docs);
+// };
